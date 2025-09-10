e747afb84b7de416c74ae59133e0d8d13eb2bae0
zeh-raan <zehraan2003@gmail.com>
2025-09-10 23:35:25 +0400

    Starting Hike CLI project

--- main.go
+++ main.go
@@ -1,17 +1,42 @@
+// go run LearnGo/main.go
 package main
 
-import "fmt" // for printing text
+import "fmt" // for printing, input
+
+// composite data type with multiple types
+type hike struct {
+	name       string
+	location   string
+	difficulty float64
+}
+
+// method in Go - attaches hike type to a function as a receiver
+func (h hike) printHike() {
+	fmt.Printf("Hike: %s\nLocation: %s\nDifficulty: %.1f\n",
+		h.name, h.location, h.difficulty)
+}
+
+// Add hikes with a slice array
+func addHike(slice []hike, h hike) []hike {
+	return append(slice, h)
+}
+
+// put values in a neat table
+func createTable() {}
 
 // must have main function
 func main() {
-	const name = "Khan"
-	p_language := "GO"
-	// %v - replace the default parameter (int, string, etc...)
-	fmt.Printf("Hello %v from %v\n", name, p_language)
-	// %s - replace the parameter(s) into a string
-	fmt.Printf("You are going to be a %s developer\n", "full stack")
-	// %d - replace the parameter(s) into a decimal
-	fmt.Printf("You have to be a %d/%d\n", 10, 10)
-	// %f - replace the parameter(s) into a float64 and significant figure can be specified
-	fmt.Printf("You are %.2f there", 99.9888)
+	var hikes []hike
+
+	// slice hard coded
+	hikes = addHike(hikes, hike{"Le Morne", "Black River", 3.5})
+
+	// loop without index through each hikes
+	for _, hike := range hikes {
+		hike.printHike()
+	}
+
+	// allocate into map with the help of make
+	// m := make(map[int]hike)
+	// m[0] = hike{}
 }
